9239996528d9dcc650c4c87d1fc800980431fe1d
David Touriño <david.tourino@outlook.com>
2021-10-12 00:06:21 +0200

    lint

--- src/integration-test/kotlin/com/davidtca/nolocal/NoLocalApplicationIntegrationTest.kt
+++ src/integration-test/kotlin/com/davidtca/nolocal/NoLocalApplicationIntegrationTest.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test
 internal class NoLocalApplicationIntegrationTest {
 
     @Test
-    //Dummy test to get Coverage
+    // Dummy test to get Coverage
     fun mainTest() {
         main(arrayOf("-server.port=0"))
     }
