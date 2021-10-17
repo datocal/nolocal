d52c31d13a39bd33a081affa0c920500bc65439b
David Touriño <david.tourino@outlook.com>
2021-10-18 00:33:53 +0200

    Remove TokenUtility companion

--- src/main/kotlin/com/davidtca/nolocal/framework/TokenUtility.kt
+++ src/main/kotlin/com/davidtca/nolocal/framework/TokenUtility.kt
@@ -1,17 +1,13 @@
 package com.davidtca.nolocal.framework
 
-class TokenUtility {
-    companion object {
-        const val TOKEN_DELIMITER = " "
-        const val BOT_PREFIX = "+"
-    }
-}
+const val TOKEN_DELIMITER = " "
+const val BOT_PREFIX = "+"
 
 fun command(message: String): String {
-    val splitMessage = message.split(TokenUtility.TOKEN_DELIMITER)
-    return splitMessage[0].removePrefix(TokenUtility.BOT_PREFIX)
+    val splitMessage = message.split(TOKEN_DELIMITER)
+    return splitMessage[0].removePrefix(BOT_PREFIX)
 }
 
 fun isCommand(message: String): Boolean {
-    return message.startsWith(TokenUtility.BOT_PREFIX)
+    return message.startsWith(BOT_PREFIX)
 }
