d88f594434f1ad28029aa53c2ea8cddeebf0a025
David Touriño <david.tourino@outlook.com>
2021-10-17 13:39:22 +0200

    docusaurus config

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -8,14 +8,14 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://davidtourino.github.io/nolocal',
+  baseUrl: '/nolocal/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'davidtourino', // Usually your GitHub org/user name.
+  projectName: 'nolocal', // Usually your repo name.
+  trailingSlash: true,
   presets: [
     [
       '@docusaurus/preset-classic',
