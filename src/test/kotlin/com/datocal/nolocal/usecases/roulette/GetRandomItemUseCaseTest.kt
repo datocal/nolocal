046a71ec58312ecc6a3585fa382e8e1fed7f24a2
David Touriño <david.tourino@outlook.com>
2021-10-22 16:44:02 +0200

    Add missing tests

--- src/test/kotlin/com/datocal/nolocal/usecases/roulette/GetRandomItemUseCaseTest.kt
+++ src/test/kotlin/com/datocal/nolocal/usecases/roulette/GetRandomItemUseCaseTest.kt
@@ -1,3 +1,18 @@
 package com.datocal.nolocal.usecases.roulette
 
-internal class GetRandomItemUseCaseTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class GetRandomItemUseCaseTest{
+
+    @Test
+    fun `should call roulette`(){
+        val useCase = GetRandomItemUseCase()
+        val request = GetRandomItemUseCaseRequest(items = listOf("a", "a", "a"))
+
+        val response = useCase.execute(request)
+
+        assertEquals("a", response.item)
+    }
+
+}
