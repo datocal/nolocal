cbdbbb875f2bab8c48cf52da3cae8125780d3af5
David Touriño <david.tourino@outlook.com>
2021-10-22 23:00:44 +0200

    Quality gate

--- src/test/kotlin/com/datocal/nolocal/framework/command/RouletteCommandRunnerTest.kt
+++ src/test/kotlin/com/datocal/nolocal/framework/command/RouletteCommandRunnerTest.kt
@@ -26,28 +26,35 @@ internal class RouletteCommandRunnerTest {
 
     @Test
     fun `should request random item with a 3-element list`() {
-        Mockito.`when`(eventMock.messageContent).thenReturn("+roulette Test \n ~~Test2~~ \n Test3")
+        Mockito.`when`(eventMock.messageContent).thenReturn("+roulette $TEST \n $TEST_2_DISCARD \n $TEST_3")
         Mockito.`when`(
             useCase.execute(
-                GetRandomItemUseCaseRequest(listOf("Test", "~~Test2~~", "Test3"))
+                GetRandomItemUseCaseRequest(listOf(TEST, TEST_2_DISCARD, TEST_3))
             )
-        ).thenReturn(GetRandomItemUseCaseResponse("Test"))
+        ).thenReturn(GetRandomItemUseCaseResponse(TEST))
 
         commandRunner.accept(eventMock)
 
-        verify(useCase).execute(request = GetRandomItemUseCaseRequest(listOf("Test", "~~Test2~~", "Test3")))
+        verify(useCase).execute(request = GetRandomItemUseCaseRequest(listOf(TEST, TEST_2_DISCARD, TEST_3)))
     }
 
     @Test
     fun `should return custom message when all elements are filtered`() {
-        Mockito.`when`(eventMock.messageContent).thenReturn("+roulette ~~Test~~ \n ~~Test2~~")
+        Mockito.`when`(eventMock.messageContent).thenReturn("+roulette $TEST_DISCARD \n $TEST_2_DISCARD")
         Mockito.`when`(
             useCase.execute(
-                GetRandomItemUseCaseRequest(listOf("~~Test~~", "~~Test2~~"))
+                GetRandomItemUseCaseRequest(listOf(TEST_DISCARD, TEST_2_DISCARD))
             )
         ).thenReturn(GetRandomItemUseCaseResponse(null))
         commandRunner.accept(eventMock)
 
         verify(channelMock).sendMessage("No items found")
+    }
+
+    companion object {
+        private const val TEST_2_DISCARD = "~~Test2~~"
+        private const val TEST_DISCARD = "~~Test~~"
+        private const val TEST = "Test"
+        private const val TEST_3 = "Test3"
     }
 }
