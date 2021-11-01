80cad3107bcbb89a8c78f3cd5a6289b977ce0e0b
David Touriño <david.tourino@outlook.com>
2021-11-01 21:54:34 +0100

    Log interactions

--- src/main/kotlin/com/datocal/nolocal/framework/controller/InteractionsController.kt
+++ src/main/kotlin/com/datocal/nolocal/framework/controller/InteractionsController.kt
@@ -1,6 +1,7 @@
 package com.datocal.nolocal.framework.controller
 
 import com.fasterxml.jackson.annotation.JsonCreator
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class InteractionsController() {
 
+    private val logger = LoggerFactory.getLogger(InteractionsController::class.java)
+
     @PostMapping("/discord/interactions")
     fun execute(@RequestBody interaction: Interaction): Interaction {
+        logger.info(interaction.toString())
         return interaction
     }
 }
