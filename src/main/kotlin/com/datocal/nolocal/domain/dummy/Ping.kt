00457d14c9fe205a1b351a3c4e6713ade94c6323
David Touriño <david.tourino@outlook.com>
2021-11-07 03:40:56 +0100

    Test change with docker

--- src/main/kotlin/com/datocal/nolocal/domain/dummy/Ping.kt
+++ src/main/kotlin/com/datocal/nolocal/domain/dummy/Ping.kt
@@ -3,6 +3,6 @@ package com.datocal.nolocal.domain.dummy
 class Ping {
 
     fun ping(request: String = "culo"): String {
-        return "El $request tuyo"
+        return "El $request suyo"
     }
 }
