10e20021c085f9834a9cfda27d7633b9a610317f
David Touriño <david.tourino@outlook.com>
2021-11-07 22:56:26 +0100

    Add logs

--- src/main/kotlin/com/datocal/nolocal/framework/discord/controller/InteractionsController.kt
+++ src/main/kotlin/com/datocal/nolocal/framework/discord/controller/InteractionsController.kt
@@ -3,6 +3,8 @@ package com.datocal.nolocal.framework.discord.controller
 import com.datocal.nolocal.framework.commands.DiscordCommandRunner
 import com.datocal.nolocal.framework.discord.model.Interaction
 import com.datocal.nolocal.framework.discord.model.InteractionResponse
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -11,10 +13,20 @@ import org.springframework.web.bind.annotation.RestController
 class InteractionsController(
     private val commands: Map<String, DiscordCommandRunner>
 ) {
+    private val logger = LoggerFactory.getLogger(InteractionsController::class.java)
+
 
     @PostMapping("/discord/interactions")
     fun execute(@RequestBody interaction: Interaction): InteractionResponse {
-        return commands[interaction.data?.name]?.accept(interaction) ?: defaultResponse()
+        logger.trace("Incoming request: $interaction")
+        val response =  commands[interaction.data?.name]?.accept(interaction) ?: defaultResponse()
+        logger.trace("Response: $response")
+        return response
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handler(exception: Exception){
+        logger.error("Error processing request: ${exception.message}", exception)
     }
 
     // ACK Discord PING checks
