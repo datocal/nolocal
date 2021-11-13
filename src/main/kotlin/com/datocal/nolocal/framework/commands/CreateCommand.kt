b8a3a82de49a630280eaa66e0a0359691403c17b
David Touriño <david.tourino@outlook.com>
2021-11-13 01:37:51 +0100

    Remove codecov ignore file

---
+++ src/main/kotlin/com/datocal/nolocal/framework/commands/CreateCommand.kt
@@ -0,0 +1,11 @@
+package com.datocal.nolocal.framework.commands
+
+import com.datocal.nolocal.framework.discord.model.Interaction
+import com.datocal.nolocal.framework.discord.model.InteractionResponse
+
+class CreateCommand : DiscordCommand {
+
+    override fun accept(interaction: Interaction): InteractionResponse {
+        TODO("Not yet implemented")
+    }
+}
