3674bff027badb02f358941b81315fdf52ede13e
David Touriño <david.tourino@outlook.com>
2022-04-12 22:31:42 +0200

    Upgrade gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,26 +3,26 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 // I'm new to gradle and kotlin so this file will need a rework, for sure...
 
 plugins {
-    id("org.springframework.boot") version "2.5.6"
+    id("org.springframework.boot") version "2.6.6"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("org.unbroken-dome.test-sets") version "4.0.0"
-    id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
-    id("info.solidsoft.pitest") version "1.7.0"
-    kotlin("jvm") version "1.5.31"
-    kotlin("plugin.spring") version "1.5.31"
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
+    id("info.solidsoft.pitest") version "1.7.4"
+    kotlin("jvm") version "1.6.10"
+    kotlin("plugin.spring") version "1.6.10"
     jacoco
 }
 
 group = "com.datocal"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
 }
 
-extra["springCloudVersion"] = "2020.0.4"
-extra["testcontainersVersion"] = "1.16.0"
+extra["springCloudVersion"] = "2021.0.1"
+extra["testcontainersVersion"] = "1.16.3"
 
 testSets {
     "integrationTest" {
@@ -43,7 +43,7 @@ tasks.jacocoTestReport {
 }
 
 pitest {
-    junit5PluginVersion.set("0.14")
+    junit5PluginVersion.set("0.15")
     testSourceSets.set(setOf(sourceSets.test.get(), testSets["integrationTest"].sourceSet))
 }
 
@@ -51,12 +51,12 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.cloud:spring-cloud-starter-openfeign:3.0.5")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign:3.1.1")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     runtimeOnly("io.micrometer:micrometer-registry-new-relic")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.testcontainers:junit-jupiter:1.16.2")
-    testImplementation("io.rest-assured:spring-mock-mvc:4.4.0")
+    testImplementation("org.testcontainers:junit-jupiter:1.16.3")
+    testImplementation("io.rest-assured:spring-mock-mvc:5.0.1")
 }
 
 dependencyManagement {
@@ -69,7 +69,7 @@ dependencyManagement {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
