69986f1d6037eb70b69a5d9fe93fb15f4938046d
David Touriño <david.tourino@outlook.com>
2022-05-02 00:59:50 +0200

    Convert beta pipeline to main pipeline

--- pipeline/init.sh
+++ pipeline/init.sh
@@ -1,13 +1,10 @@
 #! /bin/bash
 
-sudo iptables -I INPUT 6 -m state --state NEW -p tcp --match multiport --dports 443, 80 -j ACCEPT
-sudo netfilter-persistent save
+sudo apt-get update -y
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common gnupg lsb-release -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpgsudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-sudo apt-get update -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose -y
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
