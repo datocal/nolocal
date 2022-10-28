724250403aed1a9eea803987b17f2aa9f3c91569
David Touriño <david.tourino@outlook.com>
2022-10-28 20:20:24 +0200

    Add codecov.yml plus test

--- src/test/kotlin/com/datocal/nolocal/infrastructure/discord/configuration/DiscordApiConfigurationPropertiesTest.kt
+++ src/test/kotlin/com/datocal/nolocal/infrastructure/discord/configuration/DiscordApiConfigurationPropertiesTest.kt
@@ -1,3 +1,27 @@
 package com.datocal.nolocal.infrastructure.discord.configuration
 
-internal class DiscordApiConfigurationPropertiesTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class DiscordApiConfigurationPropertiesTest {
+
+    private companion object {
+        private const val EXPECTED_DISCORD_DEV_URL =
+            "https://discord.com/api/v8/applications/{client-id}/guilds/{guild-id}"
+    }
+
+    @Test
+    fun `should create url for dev environment`() {
+        val configurationProperties = `given a configuration properties of the dev environment`()
+
+        assertEquals(EXPECTED_DISCORD_DEV_URL, configurationProperties.fullBaseUrlWithPlaceholders)
+    }
+
+    private fun `given a configuration properties of the dev environment`(): DiscordApiConfigurationProperties {
+        val configurationProperties = DiscordApiConfigurationProperties()
+        configurationProperties.activeProfile = "dev"
+        configurationProperties.host = "https://discord.com"
+        configurationProperties.baseEndpoint = "/api/v8/applications"
+        return configurationProperties
+    }
+}
