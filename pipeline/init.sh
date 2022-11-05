cb8e5d04b74c197f98eb298fed3aaef0df6b6985
David Touriño <david.tourino@outlook.com>
2022-11-05 13:39:13 +0100

    Update docker-compose with s3fs

--- pipeline/init.sh
+++ pipeline/init.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 
+# $1 - AWSACCESSKEYID
+# $2 - AWSSECRETACCESSKEY
+# $3 - tenant_id
+# $4 - region-id
+
 sudo apt-get update -y
+
+# Install docker
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common gnupg lsb-release -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
