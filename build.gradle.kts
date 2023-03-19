326eaa5faaea38a7d74dc8e8433f58c72ae2abe9
David Touriño <david.tourino@outlook.com>
2023-03-20 00:31:10 +0100

    Fix pipeline jar build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 
 plugins {
@@ -67,4 +68,8 @@ fun configureMutationTests() {
     pitest {
         junit5PluginVersion.set("1.0.0")
     }
+}
+
+tasks.withType<BootJar> {
+    archiveVersion.set("")
 }
