a8db5cbd74f82af4b9a302b79fbf4a915dae8e25
David Touriño <david.tourino@outlook.com>
2023-05-06 22:58:37 +0200

    Add test for credentials command

---
+++ src/test/kotlin/com/datocal/nolocal/infrastructure/commands/CredentialsCommandIntegrationTest.kt
@@ -0,0 +1,35 @@
+package com.datocal.nolocal.infrastructure.commands
+
+import com.datocal.nolocal.infrastructure.IntegrationTest
+import io.restassured.http.ContentType
+import io.restassured.module.mockmvc.RestAssuredMockMvc
+import org.hamcrest.Matchers
+import org.junit.jupiter.api.Test
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+
+class CredentialsCommandIntegrationTest : IntegrationTest() {
+
+    private companion object {
+        private const val CREDENTIALS_REQUEST =
+            """
+                {
+                    "data": {
+                        "name" : "credentials"
+                    }
+                }
+            """
+    }
+
+    @Test
+    fun `should respond test`() {
+        RestAssuredMockMvc.given()
+            .contentType(ContentType.JSON)
+            .body(CREDENTIALS_REQUEST)
+            .`when`()
+            .post(INTERACTIONS_ENDPOINT)
+            .then()
+            .assertThat(MockMvcResultMatchers.status().isOk)
+            .body("type", Matchers.equalTo(4))
+            .body("data.content", Matchers.equalTo("test"))
+    }
+}
