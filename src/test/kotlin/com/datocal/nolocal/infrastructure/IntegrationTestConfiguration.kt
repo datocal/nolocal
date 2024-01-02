ba3de87cca8ad960e896c80798c9f0690a6539df
David Touriño <david.tourino@outlook.com>
2024-01-02 01:25:31 +0100

    Mock bean

--- src/test/kotlin/com/datocal/nolocal/infrastructure/IntegrationTestConfiguration.kt
+++ src/test/kotlin/com/datocal/nolocal/infrastructure/IntegrationTestConfiguration.kt
@@ -1,5 +1,6 @@
 package com.datocal.nolocal.infrastructure
 
+import com.oracle.bmc.auth.AuthenticationDetailsProvider
 import com.oracle.bmc.identity.IdentityClient
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.test.mock.mockito.MockBean
@@ -9,4 +10,7 @@ class IntegrationTestConfiguration {
 
     @MockBean
     private lateinit var identityClient: IdentityClient
+
+    @MockBean
+    private lateinit var authenticationDetailsProvider: AuthenticationDetailsProvider
 }
