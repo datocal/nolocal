65ffabfa6e872b469687a972925c4ee8ba1192a3
David Touriño <david.tourino@outlook.com>
2024-01-03 02:20:41 +0100

    FEATURE: Add support for webhooks-api

--- src/test/kotlin/com/datocal/nolocal/infrastructure/discord/configuration/DiscordApiConfigurationPropertiesTest.kt
+++ src/test/kotlin/com/datocal/nolocal/infrastructure/discord/configuration/DiscordApiConfigurationPropertiesTest.kt
@@ -7,30 +7,42 @@ internal class DiscordApiConfigurationPropertiesTest {
 
     private companion object {
         private const val HOST = "https://discord.com"
-        private const val BASE_ENDPOINT = "/api/v10/applications"
-        private const val EXPECTED_DISCORD_URL = "$HOST$BASE_ENDPOINT/{client-id}"
-        private const val EXPECTED_DISCORD_GUILD_URL = "$EXPECTED_DISCORD_URL/guilds/{guild-id}"
+        private const val BASE_ENDPOINT = "/api/v10"
+        private const val APPLICATIONS_BASE_ENDPOINT = "/applications"
+        private const val WEBHOOKS_BASE_ENDPOINT = "/webhooks"
+        private const val EXPECTED_WEBHOOKS_DISCORD_URL = "https://discord.com/api/v10/webhooks/{client-id}"
+        private const val EXPECTED_APPLICATIONS_DISCORD_URL = "https://discord.com/api/v10/applications/{client-id}"
+        private const val EXPECTED_APPLICATIONS_DISCORD_GUILD_URL = "https://discord.com/api/v10/applications/{client-id}/guilds/{guild-id}"
     }
 
     @Test
     fun `should create url with guild mode`() {
-        val configurationProperties = `given a configuration properties with guild mode by`()
+        val configurationProperties = `given a configuration properties with guild mode by`(guildMode = true)
 
-        assertEquals(EXPECTED_DISCORD_GUILD_URL, configurationProperties.fullBaseUrlWithPlaceholders)
+        assertEquals(EXPECTED_APPLICATIONS_DISCORD_GUILD_URL, configurationProperties.fullApplicationsUrl)
     }
 
     @Test
     fun `should create url without guild mode`() {
         val configurationProperties = `given a configuration properties with guild mode by`(guildMode = false)
 
-        assertEquals(EXPECTED_DISCORD_URL, configurationProperties.fullBaseUrlWithPlaceholders)
+        assertEquals(EXPECTED_APPLICATIONS_DISCORD_URL, configurationProperties.fullApplicationsUrl)
     }
 
-    private fun `given a configuration properties with guild mode by`(guildMode: Boolean = true): DiscordApiConfigurationProperties {
+    private fun `given a configuration properties with guild mode by`(guildMode: Boolean): DiscordApiConfigurationProperties {
         val configurationProperties = DiscordApiConfigurationProperties()
         configurationProperties.guildMode = guildMode
         configurationProperties.host = HOST
         configurationProperties.baseEndpoint = BASE_ENDPOINT
+        configurationProperties.applicationsBaseEndpoint = APPLICATIONS_BASE_ENDPOINT
+        configurationProperties.webhooksBaseEndpoint = WEBHOOKS_BASE_ENDPOINT
         return configurationProperties
+    }
+
+    @Test
+    fun `should create a webhooks url`(){
+        val configurationProperties = `given a configuration properties with guild mode by`(guildMode = false)
+
+        assertEquals(EXPECTED_WEBHOOKS_DISCORD_URL, configurationProperties.fullWebHooksUrl)
     }
 }
