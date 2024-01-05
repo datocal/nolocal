45d94f668f6685c6909f407f2a0903e8549cd3b2
David Touriño <david.tourino@outlook.com>
2024-01-06 00:57:27 +0100

    FEATURE: Add webhooks followup client

--- src/main/kotlin/com/datocal/nolocal/infrastructure/discord/client/DiscordWebhooksClient.kt
+++ src/main/kotlin/com/datocal/nolocal/infrastructure/discord/client/DiscordWebhooksClient.kt
@@ -1,7 +1,26 @@
 package com.datocal.nolocal.infrastructure.discord.client
 
+import com.datocal.nolocal.infrastructure.discord.model.ApplicationCommand
 import org.springframework.web.reactive.function.client.WebClient
 
 class DiscordWebhooksClient(
     private val client: WebClient,
+) {
+
+    fun followup(
+        interactionToken: String,
+        request: FollowUpRequest,
+    )  {
+        client
+            .post()
+            .uri("/$interactionToken")
+            .bodyValue(request)
+            .retrieve()
+            .toEntity(ApplicationCommand::class.java)
+            .block()
+    }
+}
+
+data class FollowUpRequest(
+    val content: String,
 )
